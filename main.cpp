2bdbefe1694dc1695fee614d859cd36a4496b4f3
A Student <student@nowhere.edu>
2026-06-04 05:05:06 +0000

    Jun 3, 2026, 10:05 PM

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
+bool onBoard(int row, int col)
+{
+}
+
 int main()
 {
-  cout << "Hello world" << endl;
+  int row, col, N;
+  cout << "Enter starting row (0-7): ";
+  cin >> row;
+  cout << "Enter starting column (0-7): ";
+  cin >> col;
+  cout << "Enter number of moves: ";
+  cin >> N;
 }
