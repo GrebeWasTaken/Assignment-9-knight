2709f869898fec7a2eaf03837252295bbd162f0e
A Student <student@nowhere.edu>
2026-06-04 05:29:18 +0000

    Jun 3, 2026, 10:29 PM

--- main.cpp
+++ main.cpp
@@ -15,9 +15,11 @@ void findKnightPath(int row, int col, int movesLeft){
 
   if (movesLeft == 0){
     cout << "(" << row << ", " << col << ")" << endl;
-    
+
+    return;
   }
 
+  
 }
 
 int main()
