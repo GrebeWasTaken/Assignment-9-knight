eff1e61a9e7084090463217d05f20fb2dd381179
A Student <student@nowhere.edu>
2026-06-04 05:27:05 +0000

    Jun 3, 2026, 10:27 PM

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@ bool onBoard(int row, int col)
   return row >= 0 && row < 8 && col >= 0 && col < 8;
 }
 
-void findKnightPath(int row, int col, int movesLeft)
+void findKnightPath(int row, int col, int movesLeft){
+  if (!o)
+}
 
 int main()
 {
