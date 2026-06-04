779e839a5ad14a33acea00fd19f61b2eae1343e2
A Student <student@nowhere.edu>
2026-06-04 05:54:45 +0000

    Jun 3, 2026, 10:54 PM

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@ bool onBoard(int row, int col)
 
 void printPath(int pathRows[], int pathCols[], int pathLength){
   for (int i = 0; i < pathLength; i++){
-    cout << "(" << pathRows[i] << ", " << pathCols[]
+    cout << "(" << pathRows[i] << ", " << pathCols[i] << ")";
+
+    
   }
 }
 
